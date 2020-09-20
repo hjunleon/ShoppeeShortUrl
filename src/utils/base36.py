4c26498ff643f6840f60bca19707e78afe613460
hjunleon1999 <hjunleon@gmail.com>
2020-09-20 21:01:53 +0800

    again

--- src/utils/base36.py
+++ src/utils/base36.py
@@ -8,14 +8,23 @@ def encode(base10_num: int) -> str:
     r = []
     while num > 0:
         num, rem = divmod(num, _BASE)
+        #r.insert(0,ALPHABET[rem])
         r.append(ALPHABET[rem])
-    r.reverse()
+    #r.reverse()
     return ''.join(r)
 
 
 def decode(num: str) -> int:
     r = 0
     n = len(num)
+    #print(n)
+    idx = len(num)- 1
+    while idx != -1:
+        r += R_ALPHABET[num[idx]] * (_BASE ** (n - idx - 1))
+        idx -= 1
+    
+    """
     for i, c in enumerate(num):
         r += R_ALPHABET[c] * (_BASE ** (n - i - 1))
+        """
     return r
